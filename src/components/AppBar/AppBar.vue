43611b2f1b91f381fc55c3c959ca0e8b8e2c065b
Rony Nedkov <rony.nedkov@ilent.nl>
2025-06-23 22:05:49 +0200

    improve styling

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -1,44 +1,44 @@
 <template>
   <v-app-bar flat elevation="1">
-    <div>
+    <div class="d-flex flex-row align-center">
       <v-img
-        src="@/assets/logo.svg"
+        :src="logo"
         width="40px"
         class="pa-2 ml-2 app-bar-logo"
         @click="refresh"
       />
-    </div>
-    <div class="d-flex flex-row justify-end" v-if="!isAuthenticated()">
-      <v-app-bar-title>MapStyler</v-app-bar-title>
-
+      <div class="toolbar-title">MapStyler</div>
       <v-btn
         color="black"
         size="small"
         href="https://www.oneprojectatatime.nl/tag/mapstyler/"
         target="_blank"
-        variant="plain"
+        variant="text"
         rel="noopener"
-        class="ml-5"
+        class="ml-5 mt-1"
+        v-if="!isAuthenticated()"
         >blog</v-btn
       >
-    </div>
 
-    <div v-else class="ml-4 d-flex flex-row">
-      <v-btn
-        color="black"
-        size="small"
-        @click="$router.push({ name: 'projects' })"
-        :variant="$route.name === 'projects' ? 'text' : 'plain'"
-        >projects</v-btn
-      >
-      <v-btn
-        color="black"
-        background
-        size="small"
-        @click="$router.push({ name: 'editor' })"
-        :variant="$route.name === 'editor' ? 'text' : 'plain'"
-        >editor</v-btn
-      >
+      <div v-else class="ml-4 d-flex flex-row">
+        <v-btn
+          color="black"
+          size="small"
+          class="mt-1"
+          @click="$router.push({ name: 'projects' })"
+          :variant="$route.name === 'projects' ? 'text' : 'plain'"
+          >projects</v-btn
+        >
+        <v-btn
+          color="black"
+          background
+          size="small"
+          class="mt-1"
+          @click="$router.push({ name: 'editor' })"
+          :variant="$route.name === 'editor' ? 'text' : 'plain'"
+          >editor</v-btn
+        >
+      </div>
     </div>
 
     <v-spacer></v-spacer>
@@ -80,6 +80,7 @@
 
 <script>
 import AppBarMenu from "./AppBarMenu.vue";
+import logo from "@/assets/logo.svg";
 
 // store
 import { useAuthStore } from "@/store/auth.js";
@@ -89,6 +90,11 @@ import { mapState, mapActions } from "pinia";
 export default {
   components: {
     AppBarMenu,
+  },
+  data() {
+    return {
+      logo,
+    };
   },
 
   computed: {
@@ -107,5 +113,14 @@ export default {
 <style>
 .app-bar-logo:hover {
   cursor: pointer;
+}
+
+.toolbar-title {
+  margin-left: 15px;
+  font-size: 1.25rem;
+  font-weight: 400;
+  letter-spacing: 0;
+  line-height: 1.75rem;
+  text-transform: none;
 }
 </style>
