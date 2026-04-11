f041f2881c60705fd23427b856caaf36fbd036a5
Rony Nedkov <rony.nedkov@ilent.nl>
2026-04-11 21:05:20 +0200

    Refactor download method to stylejson as input parameter

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,9 @@
-const download_stylejson = function (stylename, stylejson) {
+const download_stylejson = function (stylejson) {
   const a = document.createElement("a");
   const json = JSON.stringify(stylejson, null, 2);
   const file = new Blob([json], { type: "application/json" });
   a.href = URL.createObjectURL(file);
-  a.download = `${stylename}.json`;
+  a.download = `${stylejson.name}.json`;
   a.click();
 };
 
